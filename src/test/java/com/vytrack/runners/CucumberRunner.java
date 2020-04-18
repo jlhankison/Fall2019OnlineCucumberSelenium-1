74a10f6154ca8334fdc75d6c8cbd334ce2831ea3
jlhankison <jlhankison@gmail.com>
2020-04-18 15:25:40 -0500

    lots of modifications

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         dryRun = false,
         strict = false,
-        tags = "@activities",
+        tags = "",
         plugin = {
                 "html:target/cucumber-html-report",
                 "json:target/cucumber.json"
